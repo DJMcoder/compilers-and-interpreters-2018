f84f1131d3ed0c61072f01b1ce219774ab540cca
DJ Melisso <djm@deltavbio.com>
2018-05-25 10:53:20 -0700

    added some comments to Parser

--- JavaScriptInterpreter/parser/parser.js
+++ JavaScriptInterpreter/parser/parser.js
@@ -15,7 +15,15 @@ var while_1 = __importDefault(require("../ast/while"));
 var if_1 = __importDefault(require("../ast/if"));
 var program_1 = __importDefault(require("../ast/program"));
 var environment_1 = __importDefault(require("../environment/environment"));
+/**
+ * Parser parses an input from a Scanner and returns an Abstract Syntax Tree through
+ * its method parseProgram.
+ */
 var Parser = /** @class */ (function () {
+    /**
+     * Creates a new Parser using a Scanner, and gets the first Token
+     * @param {Scanner | string} inStream the Scanner or string which is fed into the parser
+     */
     function Parser(inStream) {
         if (inStream instanceof scanner_1.default) {
             this.scanner = inStream;
@@ -25,6 +33,10 @@ var Parser = /** @class */ (function () {
         }
         this.currentToken = this.scanner.nextToken();
     }
+    /**
+     * Compares a string to the current token and throws an error if they are different
+     * @param {string} expected the string to compare to the current token
+     */
     Parser.prototype.eat = function (expected) {
         if (this.currentToken == expected) {
             this.currentToken = this.scanner.nextToken();
@@ -34,9 +46,28 @@ var Parser = /** @class */ (function () {
                 ", expected " + expected);
         }
     };
+    /**
+     * Determines whether a string is an identifier
+     * An identifier is any string which starts with a letter and is followed by numbers or letters
+     * @param {string} str the string to evaluate as an identifier
+     * @returns true if str is an identifier;
+     *          false otherwise
+     */
     Parser.isIdentifier = function (str) {
         return scanner_1.default.isLetter(str.charAt(0));
     };
+    /**
+     * Reads in a *Value* from the input scanner
+     *
+     * *Value*s can be of the form:
+     * - `(`*Expression*`)`
+     * - `true`
+     * - `false`
+     * - *id*
+     * - *num*
+     *
+     * @returns the Expression correlating to the *Value* found
+     */
     Parser.prototype.parseValue = function () {
         // (Expression)
         if (this.currentToken.charAt(0) == "(") {
@@ -65,6 +96,15 @@ var Parser = /** @class */ (function () {
         this.eat(this.currentToken);
         return new number_1.default(res);
     };
+    /**
+     * Reads in a *NegExpr* from the input scanner
+     *
+     * *NegExpr*s can be of the form:
+     * - *Value*
+     * - `-`*Value*
+     *
+     * @returns the Expression correlating to the *NegExpr* found
+     */
     Parser.prototype.parseNegExpr = function () {
         if (this.currentToken.charAt(0) == "-") {
             this.eat("-");
@@ -72,6 +112,16 @@ var Parser = /** @class */ (function () {
         }
         return this.parseValue();
     };
+    /**
+     * Reads in a *MultExpr* from the input scanner
+     *
+     * *MultExpr*s can be of the form:
+     * - *NegExpr*
+     * - *NegExpr* `*` *NegExpr*
+     * - *NegExpr* `/` *NegExpr*
+     *
+     * @returns the Expression correlating to the *MultExpr* found
+     */
     Parser.prototype.parseMultExpr = function () {
         var res = this.parseNegExpr();
         while (this.currentToken == "*" || this.currentToken == "/") {
@@ -82,6 +132,16 @@ var Parser = /** @class */ (function () {
         }
         return res;
     };
+    /**
+     * Reads in a *AddExpr* from the input scanner
+     *
+     * *AddExpr*s can be of the form:
+     * - *MultExpr*
+     * - *MultExpr* `+` *MultExpr*
+     * - *MultExpr* `-` *MultExpr*
+     *
+     * @returns the Expression correlating to the *AddExpr* found
+     */
     Parser.prototype.parseAddExpr = function () {
         var res = this.parseMultExpr();
         while (this.currentToken == "+" || this.currentToken == "-") {
@@ -92,6 +152,20 @@ var Parser = /** @class */ (function () {
         }
         return res;
     };
+    /**
+     * Reads in a *Expression* from the input scanner
+     *
+     * *AddExpr*s can be of the form:
+     * - *AddExpr*
+     * - *AddExpr* `<` *AddExpr*
+     * - *AddExpr* `>` *AddExpr*
+     * - *AddExpr* `>=` *AddExpr*
+     * - *AddExpr* `<=` *AddExpr*
+     * - *AddExpr* `<>` *AddExpr*
+     * - *AddExpr* `=` *AddExpr*
+     *
+     * @returns the Expression correlating to the *Expression* found
+     */
     Parser.prototype.parseExpression = function () {
         var res = this.parseAddExpr();
         while (this.currentToken == "<"
@@ -107,6 +181,19 @@ var Parser = /** @class */ (function () {
         }
         return res;
     };
+    /**
+     * Reads in a *Display* Statement from the Scanner
+     * Display Statements come both of these forms:
+     *
+     * - `display` *Expression*
+     * - `display` *Expression* `read` *id*
+     *
+     * Both print the evaluated *Expression* to the console; but
+     * the second one reads a value from the user and sets it to
+     * a variable with the given identifier
+     *
+     * @returns a Display Statement correlating to the input from the Scanner
+     */
     Parser.prototype.parseDisplay = function () {
         this.eat("display");
         var expr;
@@ -125,6 +212,16 @@ var Parser = /** @class */ (function () {
         }
         return new display_1.default(expr);
     };
+    /**
+     * Reads in a *Assignment* Statement from the Scanner
+     * Assignment Statements come both of these forms:
+     *
+     * - `assign` *id* `=` *Expression*
+     *
+     * This assigns the evaluated *Expression* to the variable with the given *id*
+     *
+     * @returns a Assignment Statement correlating to the input from the Scanner
+     */
     Parser.prototype.parseAssign = function () {
         this.eat("assign");
         var toVar = this.currentToken;
@@ -132,6 +229,16 @@ var Parser = /** @class */ (function () {
         this.eat("=");
         return new assignment_1.default(toVar, this.parseExpression());
     };
+    /**
+     * Reads in a *While* Statement from the Scanner
+     * While Statements come both of these forms:
+     *
+     * - `while` *Expression* `do` *Program* `end`
+     *
+     * This evaluates the *Program* every time the *Expression* evaluates to true
+     *
+     * @returns a While Statement correlating to the input from the Scanner
+     */
     Parser.prototype.parseWhile = function () {
         this.eat("while");
         var condition = this.parseExpression();
@@ -140,6 +247,19 @@ var Parser = /** @class */ (function () {
         this.eat("end");
         return new while_1.default(condition, prog);
     };
+    /**
+     * Reads in a *If* Statement from the Scanner
+     * If Statements come both of these forms:
+     *
+     * - `if` *Expression* `then` *Program* `end`
+     * - `if` *Expression* `then` *Program* `else` *Program* `end`
+     *
+     * If the expression evaluates to `true`, then the first *Program* is evaluated;
+     * If the expression evaluates to `false` and there is a second *Program*, then it runs
+     * the second *Program*
+     *
+     * @returns an If Statement correlating to the input from the Scanner
+     */
     Parser.prototype.parseIf = function () {
         this.eat("if");
         var cond = this.parseExpression();
@@ -154,6 +274,17 @@ var Parser = /** @class */ (function () {
         this.eat("end");
         return new if_1.default(cond, then);
     };
+    /**
+     * Parses a statement depending on the current Token
+     *
+     * If the current token is `display`, it parses a Display Statement
+     * If the current token is `assign`, it parses a Assignment Statement
+     * If the current token is `while`, it parses a While Statement
+     * If the current token is `if`, it parses an If Statement
+     *
+     * @returns the Statement correlating to the given input from the Scanner
+     * @throws Error if the current token is not `display`, `assign`, `while`, or `if`
+     */
     Parser.prototype.parseStatement = function () {
         if (this.currentToken == "display")
             return this.parseDisplay();
@@ -165,6 +296,12 @@ var Parser = /** @class */ (function () {
             return this.parseIf();
         throw new Error("invalid statement: " + this.currentToken);
     };
+    /**
+     * Parses a Program, which is an array of Statements, until it reaches an endpoint
+     * An enpoint occurs when it the code ends, or it reaches the token `end` or `else`
+     *
+     * @returns the Program containing all of the Statements
+     */
     Parser.prototype.parseProgram = function () {
         var res = [];
         res.push(this.parseStatement());
@@ -173,6 +310,9 @@ var Parser = /** @class */ (function () {
         }
         return new program_1.default(res);
     };
+    /**
+     * Runs a test of the Parser, parsing the file `ParserTest.txt` in this directrory
+     */
     Parser.test = function () {
         var filename = __dirname + "/ParserTest.txt";
         try {
@@ -187,8 +327,7 @@ var Parser = /** @class */ (function () {
     return Parser;
 }());
 exports.default = Parser;
-// Main method
+// If this is file is being run, run the test
 if (require.main === module) {
     Parser.test();
 }
-module.exports = Parser;
--- JavaScriptInterpreter/parser/parser.ts
+++ JavaScriptInterpreter/parser/parser.ts
@@ -11,11 +11,20 @@ import While from "../ast/while";
 import If from "../ast/if";
 import Program from "../ast/program";
 import Environment from "../environment/environment";
+import Statement from "../ast/statement";
 
+/**
+ * Parser parses an input from a Scanner and returns an Abstract Syntax Tree through
+ * its method parseProgram.
+ */
 export default class Parser {
   scanner: Scanner;
   currentToken: string;
 
+  /**
+   * Creates a new Parser using a Scanner, and gets the first Token
+   * @param {Scanner | string} inStream the Scanner or string which is fed into the parser
+   */
   constructor(inStream: Scanner | string) {
     if (inStream instanceof Scanner) {
       this.scanner = inStream;
@@ -27,6 +36,10 @@ export default class Parser {
     this.currentToken = this.scanner.nextToken();
   }
 
+  /**
+   * Compares a string to the current token and throws an error if they are different
+   * @param {string} expected the string to compare to the current token
+   */
   eat(expected: string): void {
     if (this.currentToken == expected) {
       this.currentToken = this.scanner.nextToken();
@@ -37,10 +50,29 @@ export default class Parser {
     }
   }
 
+  /**
+   * Determines whether a string is an identifier
+   * An identifier is any string which starts with a letter and is followed by numbers or letters
+   * @param {string} str the string to evaluate as an identifier
+   * @returns true if str is an identifier;
+   *          false otherwise
+   */
   static isIdentifier(str: string): boolean {
     return Scanner.isLetter(str.charAt(0));
   }
 
+  /**
+   * Reads in a *Value* from the input scanner
+   * 
+   * *Value*s can be of the form:  
+   * - `(`*Expression*`)`  
+   * - `true`  
+   * - `false`  
+   * - *id*  
+   * - *num*  
+   * 
+   * @returns the Expression correlating to the *Value* found
+   */
   parseValue(): Expression {
     // (Expression)
     if (this.currentToken.charAt(0) == "(") {
@@ -73,6 +105,15 @@ export default class Parser {
     return new Number(res);
   }
 
+  /**
+   * Reads in a *NegExpr* from the input scanner
+   * 
+   * *NegExpr*s can be of the form:  
+   * - *Value*
+   * - `-`*Value* 
+   * 
+   * @returns the Expression correlating to the *NegExpr* found
+   */
   parseNegExpr(): Expression {
     if (this.currentToken.charAt(0) == "-")
     {
@@ -82,6 +123,16 @@ export default class Parser {
     return this.parseValue();
   }
 
+  /**
+   * Reads in a *MultExpr* from the input scanner
+   * 
+   * *MultExpr*s can be of the form:  
+   * - *NegExpr*
+   * - *NegExpr* `*` *NegExpr*
+   * - *NegExpr* `/` *NegExpr*
+   * 
+   * @returns the Expression correlating to the *MultExpr* found
+   */
   parseMultExpr(): Expression {
     var res = this.parseNegExpr();
     while (this.currentToken == "*" || this.currentToken == "/") {
@@ -93,6 +144,16 @@ export default class Parser {
     return res;
   }
 
+  /**
+   * Reads in a *AddExpr* from the input scanner
+   * 
+   * *AddExpr*s can be of the form:  
+   * - *MultExpr*
+   * - *MultExpr* `+` *MultExpr*
+   * - *MultExpr* `-` *MultExpr*
+   * 
+   * @returns the Expression correlating to the *AddExpr* found
+   */
   parseAddExpr(): Expression {
     var res = this.parseMultExpr();
     while (this.currentToken == "+" || this.currentToken == "-") {
@@ -104,6 +165,20 @@ export default class Parser {
     return res;
   }
 
+  /**
+   * Reads in a *Expression* from the input scanner
+   * 
+   * *AddExpr*s can be of the form:  
+   * - *AddExpr*
+   * - *AddExpr* `<` *AddExpr*
+   * - *AddExpr* `>` *AddExpr*
+   * - *AddExpr* `>=` *AddExpr*
+   * - *AddExpr* `<=` *AddExpr*
+   * - *AddExpr* `<>` *AddExpr*
+   * - *AddExpr* `=` *AddExpr*
+   * 
+   * @returns the Expression correlating to the *Expression* found
+   */
   parseExpression(): Expression {
     var res = this.parseAddExpr();
     while (this.currentToken == "<"
@@ -120,6 +195,19 @@ export default class Parser {
     return res;
   }
 
+  /**
+   * Reads in a *Display* Statement from the Scanner
+   * Display Statements come both of these forms:
+   * 
+   * - `display` *Expression*
+   * - `display` *Expression* `read` *id*
+   * 
+   * Both print the evaluated *Expression* to the console; but
+   * the second one reads a value from the user and sets it to
+   * a variable with the given identifier
+   * 
+   * @returns a Display Statement correlating to the input from the Scanner
+   */
   parseDisplay(): Display {
     this.eat("display")
     let expr: Expression;
@@ -139,6 +227,16 @@ export default class Parser {
     return new Display(expr);
   }
 
+  /**
+   * Reads in a *Assignment* Statement from the Scanner
+   * Assignment Statements come both of these forms:
+   * 
+   * - `assign` *id* `=` *Expression*
+   * 
+   * This assigns the evaluated *Expression* to the variable with the given *id*
+   * 
+   * @returns a Assignment Statement correlating to the input from the Scanner
+   */
   parseAssign(): Assignment {
     this.eat("assign");
     var toVar = this.currentToken;
@@ -147,6 +245,16 @@ export default class Parser {
     return new Assignment(toVar, this.parseExpression());
   }
 
+  /**
+   * Reads in a *While* Statement from the Scanner
+   * While Statements come both of these forms:
+   * 
+   * - `while` *Expression* `do` *Program* `end`  
+   * 
+   * This evaluates the *Program* every time the *Expression* evaluates to true
+   * 
+   * @returns a While Statement correlating to the input from the Scanner
+   */
   parseWhile(): While {
     this.eat("while");
     var condition = this.parseExpression();
@@ -156,6 +264,20 @@ export default class Parser {
     return new While(condition, prog);
   }
 
+
+  /**
+   * Reads in a *If* Statement from the Scanner
+   * If Statements come both of these forms:
+   * 
+   * - `if` *Expression* `then` *Program* `end`
+   * - `if` *Expression* `then` *Program* `else` *Program* `end`
+   * 
+   * If the expression evaluates to `true`, then the first *Program* is evaluated;
+   * If the expression evaluates to `false` and there is a second *Program*, then it runs 
+   * the second *Program*
+   * 
+   * @returns an If Statement correlating to the input from the Scanner
+   */
   parseIf(): If {
     this.eat("if");
     var cond = this.parseExpression();
@@ -171,7 +293,18 @@ export default class Parser {
     return new If(cond, then);
   }
 
-  parseStatement() {
+  /**
+   * Parses a statement depending on the current Token
+   * 
+   * If the current token is `display`, it parses a Display Statement
+   * If the current token is `assign`, it parses a Assignment Statement
+   * If the current token is `while`, it parses a While Statement
+   * If the current token is `if`, it parses an If Statement
+   * 
+   * @returns the Statement correlating to the given input from the Scanner
+   * @throws Error if the current token is not `display`, `assign`, `while`, or `if` 
+   */
+  parseStatement(): Statement {
     if (this.currentToken == "display") return this.parseDisplay();
     if (this.currentToken == "assign" ) return this.parseAssign();
     if (this.currentToken == "while"  ) return this.parseWhile();
@@ -180,7 +313,13 @@ export default class Parser {
     throw new Error("invalid statement: " + this.currentToken);
   }
 
-  parseProgram() {
+  /**
+   * Parses a Program, which is an array of Statements, until it reaches an endpoint
+   * An enpoint occurs when it the code ends, or it reaches the token `end` or `else`
+   * 
+   * @returns the Program containing all of the Statements
+   */
+  parseProgram(): Program {
     var res = []
     res.push(this.parseStatement());
     while (this.scanner.hasNext() && this.currentToken != "end" && this.currentToken != "else") {
@@ -189,7 +328,10 @@ export default class Parser {
     return new Program(res);
   }
 
-  static test() {
+  /**
+   * Runs a test of the Parser, parsing the file `ParserTest.txt` in this directrory
+   */
+  static test(): void {
     let filename: string =  __dirname + "/ParserTest.txt";
     try
     {
@@ -205,10 +347,8 @@ export default class Parser {
 
 }
 
-// Main method
+// If this is file is being run, run the test
 if (require.main === module)
 {
   Parser.test();
 }
-
-module.exports = Parser;
